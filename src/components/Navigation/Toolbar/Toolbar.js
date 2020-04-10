e28d18975e0343582f196dc916b838ca4365bdc6
ruicorte <ruicorte@gmail.com>
2020-04-10 11:25:50 +0100

    logo height adjusted

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = props => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <Logo height="80%"/>
         <NavigationItems />
     </header>
 );
