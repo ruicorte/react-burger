ec32398322946399909ba94205f4db414fc7dcf0
ruicorte <ruicorte@gmail.com>
2020-05-05 04:26:55 +0100

    fetching orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-
 import axios from '../../axios-orders';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
@@ -40,5 +39,46 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
+    };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFail = error => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key,
+                    });
+                }
+                console.log(fetchedOrders);
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(error => {
+                dispatch(fetchOrdersFail(error));
+            });
     };
 };
