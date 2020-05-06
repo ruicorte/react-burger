d19de7db7855358c386ac5b5521d7b0f1b737a12
ruicorte <ruicorte@gmail.com>
2020-05-06 05:09:49 +0100

    added auth form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -99,8 +99,6 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        // this.setState({ loading: true });
-
         const formData = {};
         for (let formElement in this.state.orderForm) {
             formData[formElement] = this.state.orderForm[formElement].value;
@@ -116,18 +114,37 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        // if (!rules) {
-        //     return true;
-        // }
+        /*         let isValid = true;
+                if (rules?.required) {
+                    isValid = value.trim() !== '' && isValid;
+                }
+                if (rules?.minLength) {
+                    isValid = value.length >= rules.minLength && isValid;
+                }
+                if (rules?.maxLength) {
+                    isValid = value.length <= rules.maxLength && isValid;
+                }
+                return isValid; */
         let isValid = true;
-        if (rules?.required) {
+        if (!rules) {
+            return true;
+        }
+        if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
-        if (rules?.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
-        if (rules?.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
         return isValid;
     }
